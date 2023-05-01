b4b3f33eb2e03cb3450f214b429ccc592afa956e
Francisco J. Guerrero <guerrero.francisco.jose@gmail.com>
2023-05-01 15:47:45 -0700

    Update script_physical_data_cleaning.R

--- assets/data/script_physical_data_cleaning.R
+++ assets/data/script_physical_data_cleaning.R
@@ -21,19 +21,41 @@ phys_dat_ro <- read_csv(paste(raw_data,"230430_ord_basin_hydrogeom_yrb_wrb.csv",
 
 summary(phys_dat_ro)
 
+# We find several variables with zero or missing values. We will start with missing or
+# NA values for watershed area since this variable is instrumental for scaling analysis. 
 
-################NEED TO CHANGE TO phys_dat_ro################################################
+summary(filter(phys_dat_ro, wshd_area_km2 == 0))
 
-# We find several variables with zero or missing values: 
+# 72 data points with missing values for reach slope and 22 NAs for roughness.All these 
+# values corresponding to 1st order streams with also 43 NAs in wshd_stream_dens
+
+phys_dat_trm0 <- filter(phys_dat_ro,wshd_area_km2!=0)
+
+# Checking the summary of the trimmed dataset
+
+summary(phys_dat_trm0)
+
+# We find 30 NAs values for roughness, which we will use in the estimation of residence
+# times
+
+summary(filter(phys_dat_trm0,is.na(roughness)==TRUE))
+
+
+
+
+
+
+
+
+
 
 # Total Drainage Area
 
-summary(filter(phys_dat,totdasqkm == 0))
+summary(filter(phys_dat_ro,wshd_area_km2 == 0))
 
-# All 72 missing values correspond to first order streams with zero values for catchment
-# areas as well, so we will remove these data points.
+# All 45 NA values correspond to first order with small watershed areas (close to zero).
 
-phys_dat_trm <- (filter(phys_dat,totdasqkm!=0))
+phys_dat_trm <- (filter(phys_dat_ro,wshd_stream_dens=0))
 
 summary(phys_dat_trm)
 
@@ -43,28 +65,90 @@ summary(filter(phys_dat_trm, is.na(roughness)==TRUE))
 
 # Roughness
 
-# NA values in roughness correspond to flowline_slope = 0. Otherwise, the cover
+# NA values in roughness correspond to missing reach_slope values. Otherwise, these NAs cover
 # a wide range of watershed characteristics
 
-n_plot <- ggplot(data = filter(phys_dat_trm, 
+n_plot <- ggplot(data = filter(phys_dat_trm0, 
                                is.na(roughness) == FALSE),
-                 aes(x = streamorde,
+                 aes(x = as.factor(stream_order),
                      y = roughness,
-                     color=as.factor(streamorde)))+
-  geom_boxplot()
+                     color=as.factor(stream_order)))+
+  facet_wrap(~basin,ncol = 2)+
+  geom_boxplot()+
+  theme(legend.position = "none")
 n_plot
 
-# We will replace the missing n values by the average value for the corresponding 
+# WE observe a consistence decrease of roughness with stream order, so We proceed to 
+# replace the missing n values by the average value for the corresponding 
 # stream order
 
-phys_dat_trm <- phys_dat_trm %>% 
-  group_by(streamorde) %>% 
+phys_dat_trm1 <- phys_dat_trm0 %>% 
+  group_by(stream_order,
+           basin) %>% 
   mutate(roughness_ord = mean(roughness,na.rm = TRUE)) %>% 
   ungroup() %>% 
   mutate(roughness = if_else(is.na(roughness),
                              roughness_ord,
                              roughness)) %>% 
   select(-roughness_ord)
+
+# We observe missing values (-9998) for stream slope. Se we proceed to check how 
+# many of them are and how those empty values for stream slope are related to the
+# dataset
+
+summary(filter(phys_dat_trm1,reach_slope < 0))
+
+# Since these missing values encompass multiple stream orders, we inspect the relationship
+# between stream slope and stream order
+
+slope_order <- filter(phys_dat_trm1,reach_slope>=0) %>% 
+  select(stream_order,
+         basin,
+         reach_slope) %>% 
+  ggplot(aes(x = as.factor(stream_order),
+             y = reach_slope,
+             color = as.factor(stream_order)))+
+  geom_boxplot()+
+  scale_y_log10()+
+  facet_wrap(~basin, ncol = 2)
+slope_order
+
+# We proceed to replace missing slope values with the averge for their correspondent
+# stream order
+
+phys_dat_trm2 <- phys_dat_trm1%>% 
+  group_by(stream_order,
+           basin) %>% 
+  mutate(slope_na = if_else(reach_slope<0,
+                             NA,
+                             reach_slope),
+         slope_od = if_else(is.na(slope_na),
+                            mean(slope_na,na.rm = TRUE),
+                            slope_na)) %>% 
+  ungroup() %>% 
+  mutate(reach_slope = if_else(reach_slope<0,
+                               slope_od,
+                               reach_slope))%>% 
+  select(-c(slope_na,slope_od))
+
+summary(phys_dat_trm2)
+
+
+
+
+test <- select(phys_dat_trm1,
+               comid,
+               basin,
+               stream_order,
+               reach_slope) %>% 
+  group_by(stream_order,basin) %>% 
+  mutate(slope_na = if_else(reach_slope < 0,
+                             NA,
+                             reach_slope),
+         slope_od = if_else(is.na(slope_na),
+                            mean(slope_na,na.rm = TRUE),
+                            slope_na))
+
 
 # Mean annual flow
 
@@ -80,7 +164,7 @@ phys_dat_trm <- filter(phys_dat_trm, mean_ann_flow_m3s!=0)
 # We have only 1 NA value corresponding to a 1 order stream. We proceed to remove this
 # row
 
-phys_dat_trm <- filter(phys_dat_trm, is.na(stream_dens)==FALSE)
+phys_dat_trm <- filter(phys_dat_trm, is.na(ctch_stream_dens)==FALSE)
 
 # Stream slope & Stream length (we replace zero values with the corresponding flow line values)
 
