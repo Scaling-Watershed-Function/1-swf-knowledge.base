9d539cec27f4efb4ede08e2d65a7a9c624ec6418
Francisco J. Guerrero <97982871+guerrero-fj@users.noreply.github.com>
2023-06-28 09:10:17 -0700

    Updated RSelenium Script for system preparation

--- assets/data/data_download_rselenium/script_system_prep_rselenium.R
+++ assets/data/data_download_rselenium/script_system_prep_rselenium.R
@@ -51,12 +51,27 @@
 
 # You can check if you have installed versions of the packages by using `require()`
 
+# Alternatively, you can run: #install.packages('librarian') and then add those packages
+# to the list below. The advantage of using librarian is that if the packages are not 
+# installed in your computer, it would download, install, and call these packages. 
+
 librarian::shelf(tidyverse,
                  RSelenium,
                  netstat,
                  wdman,
                  rvest,
-                 data.table)
+                 data.table,
+                 Rcpp,
+                 sys,
+                 openssl,
+                 jsonlite,
+                 curl,
+                 ps,
+                 httr,
+                 yaml,
+                 processx,
+                 netstat,
+                 binman)
 
 # SETTING UP SELENIUM
 
@@ -67,22 +82,37 @@ selenium()
 # `selenium_object`
 selenium_object <- selenium(retcommand = T, check = F)
 
-# ATENTION! Here, there is a critical step you need to do outside of R. The newest
+################################ ATENTION! ######################################
+# Here, there is a critical step you need to do outside of R. The newest
 # Chrome drivers include a LICENSE files, that cause issues when trying 
 # to open a chrome browser using RSelenium. You need to delete those files.
 
+# You can check the chrome versions installed in your system by running:
+
+binman::list_versions("chromedriver")
+
 # First, while still in R, you can locate the path to those files by opening
 # the selenium object you just created. 
+
+selenium_object
 
 # This object contains the paths to all the drivers installed, included those 
 # for Google Chrome. The path to those files should look something like:
 
 # C:\Users\your_user.name\AppData\Local\binman\binman_chromedriver\win32 (Windows)
+
 # /Users/your.user.name/Library/Application Support/binman_chromedriver/mac64/ (Mac OS)
 
+# Copy the path and paste it in your files explorer (Windows) or Finder (MacOS) - once
+# in Finder, press simultaneously Shift + Command + G and paste the folder path. 
+
 # You need to navigate to the Application folder and locate the chromedriver files.
+
 # You will find as many folders as google chrome drivers. In that case, open each folder
 # and delete the LICENSE files.
+
+# REMEMBER to repeat this steps every time you update your browser. If you forget 
+# and try to work with RSelenium, it would return an error prompting you to do so. 
 
 # OPENING A CLIENT-SERVER OBJECT
 
@@ -106,8 +136,22 @@ rs_driver_object <- rsDriver(browser = "chrome",
                              verbose = FALSE,
                              port = free_port())
 
-# Opening a Client Browser (to be used for webscrapping)
+# It would appear as an additional chrome browser, regardless if you have one
+# already open or not. 
+
+# You will see a "data" tab open with a blank page. You will 
+# see a message right below the search bar like this: "Chrome is being controlled
+# by automatic test software".
+
+# You can test searchability by picking a target URL and running the lines:
+
+target_url <- "https://scholar.google.com/"
+
+# Opening a Client Browser (to be used for web scrapping)
 remDr <- rs_driver_object$client
+
+# Navigate to your desired url
+remDr$navigate(target_url)
 
 # Closing the Client Browser
 remDr$close()
@@ -116,9 +160,25 @@ remDr$close()
 # Stop the Selenium server and close the browser
 rs_driver_object$server$stop()
 
+# This last line should return "TRUE" which means that you have effectively closed the
+# server, so it can be open again in another instance. Otherwise, if you try to 
+# open another server with a different script, you will get an error message saying that 
+# the server is already in use and won't let you navigate to your desired URLs.
+
+# In Summary
+
 # The lines above will open a browser on a separate window, in this case it would
-# be chrome, but you can specify other browsers like safari, or firefox. You will 
-# see a message right below the search bar like this: "Chrome is being controlled
-# by automatic test software". This is because, the original purpose of RSelenium
-# is to help developers to privately test their applications online. Yet, the 
-# use of this approach for webscrapping and data download, came as a bonus. 
+# be chrome, but you can specify other browsers like safari, or Firefox. The message:
+#"Chrome is being controlled by automatic test software" appears because the original 
+# purpose of RSelenium is to help developers to privately test their applications online. 
+# Yet, the use of this approach for web scrapping and data download, came as a bonus. 
+
+
+# If everything ran smoothly with this code, you should be ready to go and try the 
+# script: script_ess_dive_rselenium_download.R
+
+
+# Happy automated downloading and happy coding!
+
+#"Reproducibility is the key to open, equitable, and accessible science. From 
+# people for people"
