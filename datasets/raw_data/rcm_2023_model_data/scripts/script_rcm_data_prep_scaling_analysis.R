030b6c6d850a3e5afa2d12a8b6afc61640e524cc
Francisco J. Guerrero <97982871+guerrero-fj@users.noreply.github.com>
2023-10-07 19:58:36 -0700

    Update script_rcm_data_prep_scaling_analysis.R

--- datasets/raw_data/rcm_2023_model_data/scripts/script_rcm_data_prep_scaling_analysis.R
+++ datasets/raw_data/rcm_2023_model_data/scripts/script_rcm_data_prep_scaling_analysis.R
@@ -11,7 +11,8 @@ librarian::shelf(tidyverse,
                  sf,
                  leaflet,
                  stringr,
-                 GGally)
+                 GGally,
+                 entropy)
 
 # Local Import-Export
 source_data <- "../../raw_data"
@@ -85,7 +86,9 @@ rf_hyporheic_dat <- merge(rf_hyporheic_flux_dat,
                        by = "comid",
                        all.x = TRUE)
 
+################################################################################
 # Comparing model input data
+################################################################################
 rcm_23_model_inp_dat <- merge(substrate_inputs,
                               rf_hyporheic_dat,
                               by = "comid",
@@ -295,32 +298,199 @@ test_dat_connectivity <- rcm_23_model_dat %>%
 test_dat_connectivity
 
 # Calculating cumulative values
+# 
+# # Recalculating cumulative variables
+# rcm_23_model_dat <- rcm_23_model_dat %>% 
+#   mutate(doc_load_kg_d = doc_stream_mg_l*mean_ann_flow_m3s*86400,
+#          do_load_kg_d = do_stream_mg_l*mean_ann_flow_m3s*86400,
+#          no3_load_kg_d = no3_stream_mg_l*mean_ann_flow_m3s*86400) %>% 
+#   group_by(basin) %>% 
+#     mutate(across(c(totco2g_day,
+#                   totco2_ang_day,
+#                   totco2_o2g_day,
+#                   tot_stream_length_km,
+#                   tot_rt_hz_s,
+#                   water_exchng_kg_day,
+#                   doc_load_kg_d,
+#                   do_load_kg_d,
+#                   no3_load_kg_d,
+#                   mean_ann_runf_mm), ~ calculate_arbolate_sum(data.frame(ID = comid,
+#                                                                        toID = tocomid,
+#                                                                        length = .x))) %>% 
+#            set_names(paste0("accm_", names(select(., totco2g_day:mean_ann_runf_mm))))) %>% 
+#   ungroup()
+# 
+# # Variability in sediment structure
+# 
+# qlabel <- c("Q10","Q20","Q30","Q40","Q50","Q60","Q70","Q80+")
+# 
+# rcm_23_model_dat <- rcm_23_model_dat %>% 
+#   mutate(accm_d50_m = calculate_arbolate_sum(data.frame(ID = comid,
+#                                                         toID = tocomid,
+#                                                         length = d50_m)),
+#          inc_comid = 1,
+#          accm_inc_comid = calculate_arbolate_sum(data.frame(ID = comid,
+#                                                             toID = tocomid,
+#                                                             length = inc_comid)),
+#          avg_d50_m = accm_d50_m/accm_inc_comid,
+#          d50_m_dist = (d50_m - avg_d50_m)^2,
+#          tot_d50_m_dist = calculate_arbolate_sum(data.frame(ID = comid,
+#                                                            toID = tocomid,
+#                                                            length = d50_m_dist)),
+#          accm_d50_m_sd = tot_d50_m_dist/(accm_inc_comid)+runif(length(tot_d50_m_dist), min = 0.0000000001, max = 0.00000001),
+#          avg_reach_slope = accm_reach_slope/accm_inc_comid,
+#          reach_slope_dist = (reach_slope - avg_reach_slope)^2,
+#          tot_reach_slope_dist = calculate_arbolate_sum(data.frame(ID = comid,
+#                                                             toID = tocomid,
+#                                                             length = reach_slope_dist)),
+#          accm_reach_slope_sd = tot_reach_slope_dist/(accm_inc_comid)+runif(length(tot_d50_m_dist), min = 0.0000000001, max = 0.00000001),
+#          slope_sd_cat = factor(Hmisc::cut2(accm_reach_slope_sd, g = 8),labels = qlabel))
+# 
+# 
+# 
+# p <- ggplot(data = rcm_23_model_dat,
+#             aes(x = accm_water_exchng_kg_day,
+#                 y = accm_totco2_ang_day,
+#                 color = accm_d50_m_sd))+
+#   geom_point(alpha = 0.5)+
+#   scale_x_log10()+
+#   scale_y_log10()+
+#   facet_wrap(~basin, ncol = 2)
+# p
+# 
+# 
+# p <- ggplot(data = rcm_23_model_dat,
+#             aes(x = wshd_area_km2,
+#                 y = accm_totco2_o2g_day/wshd_area_km2,
+#                 color = slope_sd_cat))+
+#   geom_point(alpha = 0.5)+
+#   scale_x_log10()+
+#   scale_y_log10()+
+#   geom_abline(slope = 1)+
+#   facet_wrap(basin~slope_sd_cat, ncol = 8)
+# p
+# # Saving the data
+# 
+# # Removing cumulative columns that do not add new information to respiration patterns:
+# 
+# rcm_23_model_dat <- rcm_23_model_dat %>% 
+#   select(-c(accm_d50_m,
+#             accm_d50_m_sd,
+#             accm_reach_slope,
+#             accm_reach_slope_sd,
+#             avg_d50_m,
+#             avg_reach_slope,
+#             d50_m_dist,
+#             reach_slope_dist,
+#             tot_d50_m_dist,
+#             tot_reach_slope_dist,
+#             slope_sd_cat))
 
-# Recalculating wshd stream density, and cumulative variables
+
+# Calculating quantiles
+
+#Calculating the quantiles with Hmisc::cut2, which allows for the inclusion of zeroes
+
+# https://stackoverflow.com/questions/46750635/cut-and-quantile-in-r-in-not-including-zero
+
+qlabel <- c("Q10","Q20","Q30","Q40","Q50","Q60","Q70","Q80+")
+
 rcm_23_model_dat <- rcm_23_model_dat %>% 
   group_by(basin) %>% 
-  mutate(across(c(totco2g_day,
-                  totco2_ang_day,
-                  totco2_o2g_day,
-                  tot_stream_length_km,
-                  tot_rt_hz_s,
-                  water_exchng_kg_day), ~ calculate_arbolate_sum(data.frame(ID = comid,
-                                                                       toID = tocomid,
-                                                                       length = .x))) %>% 
-           set_names(paste0("accm_", names(select(., totco2g_day:water_exchng_kg_day))))) %>% 
-  ungroup()
+  mutate(ent_cat_w = factor(Hmisc::cut2(w_hrel, g = 8),labels = qlabel),
+         ent_cat_c = factor(Hmisc::cut2(c_hrel, g = 8),labels = qlabel),
+         rst_cat = factor(Hmisc::cut2(tot_rt_hz_s, g = 8),labels = qlabel),
+         hzt_cat = factor(Hmisc::cut2(tot_q_hz_ms, g = 8),labels = qlabel),
+         pct_cat = factor(Hmisc::cut2(mean_ann_pcpt_mm, g = 8),labels = qlabel),
+         rnf_cat = factor(Hmisc::cut2(mean_ann_runf_mm, g = 8),labels = qlabel),
+         d50_cat = factor(Hmisc::cut2(d50_m, g = 8),labels = qlabel),
+         accm_hzt_cat = factor(Hmisc::cut2(accm_water_exchng_kg_day, g = 8),labels = qlabel),
+         doc_stream_mg_l = ifelse(basin == "yakima", doc_stream_mg_l + runif(length(doc_stream_mg_l), min = 0, max = 0.001), doc_stream_mg_l),
+         doc_cat = factor(Hmisc::cut2(doc_stream_mg_l, g = 8, ties.method = "min"), labels = qlabel),
+         sto_fct = as.factor(stream_order),
+         forest_scp_3 = w_forest_scp + w_water_scp,
+         humans_scp_3 = w_human_scp,
+         shrubl_scp_3 = w_shrub_scp + w_grass_scp + w_barren_scp,
+         frst_cat = factor(Hmisc::cut2(forest_scp_3 , g = 8),labels = qlabel)) %>% 
+ungroup() %>% 
+  mutate(basin_cat = as.factor(if_else(basin == "yakima",
+                                       "Yakima River (Dry)",
+                                       "Willamette River (Wet)")))
+
+
+rcm_23_model_dat <- rcm_23_model_dat %>% 
+  group_by(basin) %>% 
+  rowwise() %>% 
+  mutate(ht_3 = entropy(c(forest_scp_3,
+                          shrubl_scp_3,
+                          humans_scp_3),
+                        unit = "log2"),
+         hmax_3 = log(3,2),
+         hrel_3 = ht_3/hmax_3,
+         hrel_3u = hrel_3 + runif(1, 0.000001, 0.00001)) %>% 
+  ungroup() %>% 
+  group_by(basin) %>% 
+  mutate(hrel_3_cat = factor(Hmisc::cut2(hrel_3u , g = 8),labels = qlabel))
+
+
+my_mcolors <- c("#ffd6e8","#ffafd2","#ff7eb6","#ee5396",
+                "#d62670","#9f1853","#740937","#510224")
 
 p <- ggplot(data = rcm_23_model_dat,
-            aes(x = accm_water_exchng_kg_day,
-                y = accm_totco2_ang_day,
-                color = w_human_scp))+
+            aes(x = wshd_area_km2,
+                y = accm_totco2_o2g_day/wshd_area_km2,
+                color = frst_cat))+
   geom_point(alpha = 0.5)+
   scale_x_log10()+
   scale_y_log10()+
-  facet_wrap(~basin, ncol = 2)
+  # scale_color_manual(values = my_mcolors)+
+  scale_color_brewer(palette = "PRGn")+
+  geom_abline(slope = 1)+
+  facet_wrap(basin~hrel_3_cat, nrow = 2)
 p
 
-# Saving the data
+
+# Calculating cumulative entropy
+
+test_df <- rcm_23_model_dat %>% 
+  select(basin,
+         comid,
+         tocomid,
+         wshd_area_km2,
+         stream_order)
+
+associate_area <- function(input_df) {
+  result_df <- input_df %>%
+    group_by(basin) %>%
+    mutate(
+      max_area = max(wshd_area_km2),
+      down_wshd_area_km2 = case_when(
+        tocomid == 0 ~ wshd_area_km2,
+        wshd_area_km2 == max_area ~ wshd_area_km2,
+        TRUE ~ lag(wshd_area_km2)
+      )
+    ) %>%
+    select(basin,comid, tocomid, down_wshd_area_km2, -max_area) %>%
+    ungroup()
+  
+  return(result_df)
+}
+
+# Usage
+result <- associate_area(test_df)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 write.csv(rcm_23_model_dat,paste(local_data,"rcm_23_model_data.csv",sep = '/'),
           row.names = FALSE)
